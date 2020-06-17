23827d5bc10716e0ac75cd340462617e7a4c0208
Sudhanshu Kumar <sudhanshu.kumar.eee16@itbhu.ac.in>
2020-06-17 10:52:39 +0530

    toggle button worling now

--- mock_site/after/js/script.js
+++ mock_site/after/js/script.js
@@ -2,7 +2,7 @@ $(function(){
 	$("navbarToggle").blur(function(event){
 		var screenwidth= window.innerWidth;
 		if(screenwidth<768){
-			$("#collapsable-nav").collapse('hide');
+			$("#navbarSupportedContent").collapse('hide');
 		}
 	});
 
